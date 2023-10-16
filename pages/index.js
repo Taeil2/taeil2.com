e79aa6f031edf01da314e28042357fcb139781ab
Taeil Kwak <taeil@anvilogic.com>
2023-10-16 09:30:16 -0700

    changed url name

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>taeil2.com</title>
+        <title>tae2.com</title>
         <meta name="description" content="Taeil's Capital One bank account information" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
         <h1>
-          Welcome to <span className={styles.blueText}>taeil2.com</span>
+          Welcome to <span className={styles.blueText}>tae2.com</span>
         </h1>
         <div className={styles.paymentMethods}>
           <div>
