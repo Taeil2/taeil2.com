ea5fede695041f5ebe9bbc5e7ad622c07fc27cf7
Taeil Kwak <taeil@anvilogic.com>
2023-11-04 18:18:13 -0700

    setting up additional footer links

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,23 @@
 import Link from 'next/link'
 import styles from './footer.module.scss'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 export default function Footer() {
   return <footer className={styles.footer}>
+    <Link href="#" onClick={copyToClipboard()}>
+      <a className={styles.footerLink}>
+        <CopyToClipboard text="taeil2@gmail.com"
+          onCopy={() => console.log("copied")}>
+            <span>Email</span>
+        </CopyToClipboard>
+      </a>
+    </Link>
+    <Link href="https://www.linkedin.com/in/taeil2/">
+      <a className={styles.footerLink}>LinkedIn</a>
+    </Link>
+    <Link href="https://www.facebook.com/taeil2">
+      <a className={styles.footerLink}>Facebook</a>
+    </Link>
     <Link href="https://taeil2.notion.site/Eddy-728e9e0c7a3c40a2a5b5239652cd716f">
       <a className={styles.footerLink}>Eddy</a>
     </Link>
@@ -10,5 +25,4 @@ export default function Footer() {
       <a className={styles.footerLink}>Maps</a>
     </Link>
   </footer>
-  
 }
