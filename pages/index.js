3cceeeb6f77757b77a2be7b9aa50e1f944cda6d3
Taeil2 <taeil2@gmail.com>
2025-09-08 08:27:26 -0500

    updated site with new instructions

--- pages/index.js
+++ pages/index.js
@@ -4,86 +4,201 @@ import styles from "./index.module.scss";
 
 import Footer from "../components/footer";
 
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
 import Image from "next/image";
 
 export default function Home() {
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [fadeOut, setFadeOut] = useState(false);
+
   return (
     <div>
       <Head>
-        <title>Taeil&apos;s Bank Account Information</title>
+        <title>Send money for Justice / The Resolution / The Solution</title>
         <meta
           name="description"
-          content="Taeil's Bank Account Information"
+          content="Send money for Justice / The Resolution / The Solution"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1>
-          Welcome to <span className={styles.blueText}>tae2.com</span>
-        </h1>
-        <div className={styles.paymentMethods}>
-          <div className={styles.buseyBank}>
-            <div
-              // className={`${styles.bankLogo} ${styles.buseyBankLogo}`}
-              className={styles.bankLogo}
-              >
-              <Image
-                alt="Busey Bank"
-                src="/payment/buseybank-logo.svg"
-                width={169}
-                height={57}
-                className={styles.buseyBankLogo}
-              />
+        <h1>To send the money</h1>
+        <div className={styles.row}>
+          <div className={styles.col}>
+            <div className={styles.section}>
+              <h2>Send a new account with money in it</h2>
+              <ul>
+                <li>
+                  Create a new account at any bank or in any app that holds
+                  money.
+                  <br />
+                  Some examples include:
+                </li>
+                <li>
+                  note to self: make sure all have clear create account
+                  processes
+                  <ul>
+                    <li>JPMorgan Chase</li>
+                    <li>HSBC Holdings</li>
+                    <li>Agricultural Bank of China</li>
+                    <li>Royal Bank of Canada</li>
+                    <li>BNP Paribas</li>
+                    <li>Another Asian Bank</li>
+                    <li>Venmo</li>
+                    <li>Cash App</li>
+                  </ul>
+                </li>
+                <li>Transfer your money into this account</li>
+                <li>
+                  Email Taeil at{" "}
+                  <a className={styles.link}>
+                    <CopyToClipboard
+                      text="taeil2@gmail.com"
+                      onCopy={() => {
+                        setShowTooltip(true);
+                        setTimeout(() => {
+                          setFadeOut(true);
+                        }, 4000);
+                        setTimeout(() => {
+                          setShowTooltip(false);
+                          setFadeOut(false);
+                        }, 5000);
+                      }}
+                    >
+                      <span>taeil2@gmail.com</span>
+                    </CopyToClipboard>{" "}
+                    <span
+                      className={`${styles.tooltip} ${
+                        showTooltip && styles.showTooltip
+                      } ${fadeOut && styles.fadeOut}`}
+                    >
+                      {/* <span> */}
+                      copied taeil2@gmail.com to clipboard
+                      {/* </span> */}
+                    </span>
+                  </a>
+                  with the following:
+                  <ul>
+                    <li>The bank or app name</li>
+                    <li>The bank or app website</li>
+                    <li>Your login</li>
+                    <li>Your password</li>
+                    <li>
+                      Any other required information to access this account
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  Note: This account is not yours after sending this
+                  information.
+                </li>
+              </ul>
             </div>
-            <h2>Account Number</h2>
-            <h3>
-              2305
-              <span className={styles.spacer} />
-              8777
-              <span className={styles.spacer} />
-              7034
-            </h3>
-            <h2>Routing Number</h2>
-            <h3>
-              071
-              <span className={styles.spacer} />
-              102
-              <span className={styles.spacer} />
-              568
-            </h3>
-          </div>
-          <div className={styles.bylineBank}>
-            <div
-              // className={`${styles.bankLogo} ${styles.bylineBankLogo}`}
-              className={styles.bankLogo}
-              >
-              <Image
-                alt="Byline Bank"
-                src="/payment/bylinebank-logo.svg"
-                width={250}
-                height={68}
-                className={styles.bylineBankLogo}
-              />
+            <div className={styles.section}>
+              <h2>Send a wire transfer</h2>
+              <div className={styles.buseyBank}>
+                <div
+                  // className={`${styles.bankLogo} ${styles.buseyBankLogo}`}
+                  className={styles.bankLogo}
+                >
+                  <Image
+                    alt="Busey Bank"
+                    src="/payment/buseybank-logo.svg"
+                    width={169}
+                    height={57}
+                    className={styles.buseyBankLogo}
+                  />
+                </div>
+                <h3>Account Number</h3>
+                <h4>
+                  2305
+                  <span className={styles.spacer} />
+                  8777
+                  <span className={styles.spacer} />
+                  7034
+                </h4>
+                <h3>Routing Number</h3>
+                <h4>
+                  071
+                  <span className={styles.spacer} />
+                  102
+                  <span className={styles.spacer} />
+                  568
+                </h4>
+              </div>
             </div>
-            <h2>Account Number</h2>
-            <h3>
-              645
-              <span className={styles.spacer} />
-              079
-              <span className={styles.spacer} />
-              0874
-            </h3>
-            <h2>Routing Number</h2>
-            <h3>
-              071
-              <span className={styles.spacer} />
-              001
-              <span className={styles.spacer} />
-              533
-              <span className={styles.spacer} />
-            </h3>
           </div>
-          {/* <div className={styles.chime}>
+          <div className={styles.col}>
+            <div className={styles.section}>
+              <h2>Send through Venmo or Cash App</h2>
+              <div className={styles.venmo}>
+                <div className={styles.bankLogo}>
+                  <Image
+                    alt="Venmo"
+                    src="/payment/venmo-logo.svg"
+                    width={194.47}
+                    height={35.72}
+                    className={styles.venmoLogo}
+                  />
+                </div>
+                <div className={styles.venmoQR}>
+                  <Image
+                    alt="Venmo QR"
+                    src="/payment/venmo-qr.png"
+                    width={171}
+                    height={170}
+                  />
+                </div>
+                <p>@taeil2</p>
+                {/* <h3>Account Number</h3>
+              <h4>2221<span className={styles.spacer} />8586<span className={styles.spacer} />0055</h4>
+              <h3>Routing Number</h3>
+              <h4>031<span className={styles.spacer} />101<span className={styles.spacer} />279</h4> */}
+              </div>
+              <div className={styles.cashApp}>
+                <div className={styles.bankLogo}>
+                  <Image
+                    alt="Cashapp"
+                    src="/payment/cashapp-logo.svg"
+                    width={866.6}
+                    height={204.7}
+                    className={styles.cashAppLogo}
+                  />
+                </div>
+                <div className={styles.cashAppQR}>
+                  <Image
+                    alt="Cash App QR"
+                    src="/payment/cashapp-qr.png"
+                    width={288}
+                    height={288}
+                  />
+                </div>
+                <p>$taeilkwak</p>
+                {/* <h3>Account Number</h3>
+              <h4>
+                129
+                <span className={styles.spacer} />
+                389
+                <span className={styles.spacer} />
+                102
+                <span className={styles.spacer} />
+                7716
+              </h4>
+              <h3>Routing Number</h3>
+              <h4>
+                041
+                <span className={styles.spacer} />
+                215
+                <span className={styles.spacer} />
+                663
+              </h4> */}
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className={styles.chime}>
             <div className={styles.bankLogo}>
               <Image
                 alt="Chime"
@@ -93,113 +208,24 @@ export default function Home() {
                 className={styles.chimeLogo}
               />
             </div>
-            <h2>Account Number</h2>
-            <h3>
+            <h3>Account Number</h3>
+            <h4>
               6891
               <span className={styles.spacer} />
               5536
               <span className={styles.spacer} />
               2031
-            </h3>
-            <h2>Routing Number</h2>
-            <h3>
+            </h4>
+            <h3>Routing Number</h3>
+            <h4>
               103
               <span className={styles.spacer} />
               100
               <span className={styles.spacer} />
               195
-            </h3>
+            </h4>
           </div> */}
-          {/* <div className={styles.cashApp}>
-            <div className={styles.bankLogo}>
-              <Image
-                alt="Cashapp"
-                src="/payment/cashapp-logo.svg"
-                width={866.6}
-                height={204.7}
-                className={styles.cashAppLogo}
-              />
-            </div>
-            <div className={styles.cashAppQR}>
-              <Image
-                alt="Cash App QR"
-                src="/payment/cashapp-qr.png"
-                width={288}
-                height={288}
-              />
-            </div>
-            <p>$taeilkwak</p>
-            <h2>Account Number</h2>
-            <h3>
-              129
-              <span className={styles.spacer} />
-              389
-              <span className={styles.spacer} />
-              102
-              <span className={styles.spacer} />
-              7716
-            </h3>
-            <h2>Routing Number</h2>
-            <h3>
-              041
-              <span className={styles.spacer} />
-              215
-              <span className={styles.spacer} />
-              663
-            </h3>
-          </div> */}
-          {/* <div className={styles.bankOfAmerica}>
-            <div className={styles.bankLogo}>
-              <Image
-                alt="Bank of America"
-                src="/payment/bankofamerica-logo.svg"
-                width={720}
-                height={73}
-                className={styles.bankOfAmericaLogo}
-              />
-            </div>
-            <h2>Account Number</h2>
-            <h3>
-              0004
-              <span className={styles.spacer} />
-              7897
-              <span className={styles.spacer} />
-              6487
-            </h3>
-            <h2>Routing Number</h2>
-            <h3>
-              026
-              <span className={styles.spacer} />
-              009
-              <span className={styles.spacer} />
-              593
-            </h3>
-          </div> */}
-          {/* <div className={styles.venmo}>
-            <div className={styles.bankLogo}>
-              <Image
-                alt="Venmo"
-                src="/payment/venmo-logo.svg"
-                width={194.47}
-                height={35.72}
-                className={styles.venmoLogo}
-              />
-            </div>
-            <div className={styles.venmoQR}>
-              <Image
-                alt="Venmo QR"
-                src="/payment/venmo-qr.png"
-                width={171}
-                height={170}
-              />
-            </div>
-            <p>@taeil2</p>
-            <h2>Account Number</h2>
-            <h3>2221 8586 0055</h3>
-            <h2>Routing Number</h2>
-            <h3>031 101 279</h3>
-          </div> */}
-          {/* <div className={styles.capitalOne}>
+        {/* <div className={styles.capitalOne}>
             <div className={styles.bankLogo}>
               <Image
                 alt="Capital One"
@@ -209,26 +235,24 @@ export default function Home() {
                 className={styles.capitalOneLogo}
               />
             </div>
-            <h2>Account Number</h2>
-            <h3>
+            <h3>Account Number</h3>
+            <h4>
               361
               <span className={styles.spacer} />
               8094
               <span className={styles.spacer} />
               0070
-            </h3>
+            </h4>
             <h2>Routing Number</h2>
-            <h3>
+            <h4>
               031
               <span className={styles.spacer} />
               176
               <span className={styles.spacer} />
               110
-            </h3>
+            </h4>
           </div> */}
-        </div>
       </main>
-      <Footer />
     </div>
   );
 }
