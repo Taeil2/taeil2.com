e083f2d30afb6d584c66fd1730b280bfb34da7aa
Taeil2 <taeil2@gmail.com>
2025-09-08 08:53:02 -0500

    fixed a bullet

--- pages/index.js
+++ pages/index.js
@@ -37,8 +37,6 @@ export default function Home() {
                   money.
                   <br />
                   Some examples include:
-                </li>
-                <li>
                   <ul>
                     <li>
                       <Link href="https://www.chase.com/">Chase Bank</Link>
